4fc688c1240f1f5820cf6d050007888588823677
Nikita Kazeev <kazeevn@gmail.com>
2024-06-04 19:10:30 +0800

    Configuation name saving

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from wyckoff_transformer.trainer import train_from_config
 
 def main():
     parser = argparse.ArgumentParser(description='Train a model')
-    parser.add_argument("config", type=OmegaConf.load, help="The configuration file")
+    parser.add_argument("config", type=Path, help="The configuration file")
     parser.add_argument("dataset", type=str, default="mp_20_biternary", help="Dataset to use")
     parser.add_argument("device", type=torch.device, help="Device to train on")
     parser.add_argument("--debug", action="store_true", help="Debug mode")
@@ -23,16 +23,18 @@ def main():
     if args.device.type == "cuda":
         # UserWarning: TensorFloat32 tensor cores for float32 matrix multiplication available but not enabled. Consider setting `torch.set_float32_matmul_precision('high')` for better performance.
         torch.set_float32_matmul_precision('high')
+        
+    config = OmegaConf.load(args.config)
+    config['name'] = args.config.stem
+    config['dataset'] = args.dataset
 
-    tokeniser_config_path = Path(__file__).parent.resolve() / "yamls" / "tokenisers" / f"{args.config.tokeniser.name}.yaml"
+    tokeniser_config_path = Path(__file__).parent.resolve() / "yamls" / "tokenisers" / f"{config.tokeniser.name}.yaml"
     tokeniser_config = OmegaConf.load(tokeniser_config_path)
     if len(tokeniser_config.augmented_token_fields) > 1:
         raise ValueError("Only one augmented field is supported")
-    config = args.config
     config['tokeniser'] = tokeniser_config
-    config['dataset'] = args.dataset
 
-    wandb_config = OmegaConf.to_container(args.config)
+    wandb_config = OmegaConf.to_container(config)
     with wandb.init(
         project="WyckoffTransformer",
         job_type="train",
