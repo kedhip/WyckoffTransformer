1fee08fa04817fbfecf2bc32771b3c23d25dc68d
Nikita Kazeev <kazeevn@gmail.com>
2024-06-18 15:35:22 +0800

    v6 experiments

--- run_all_yamls.sh
+++ run_all_yamls.sh
@@ -3,4 +3,4 @@
 # pilot is prone to SMACT timeouts, so no
 # ls yamls/models/NextToken/v5/*.yaml | parallel --tmux -j4 'export CUDA_VISIBLE_DEVICES=$(({%}%2)); python train.py {} mp_20_biternary cuda --pilot && python train.py {} mp_20_biternary cuda'
 # ls yamls/models/NextToken/v5/*.yaml | parallel --tmux -j4 'export CUDA_VISIBLE_DEVICES=$(({%}%2)); python train.py {} mp_20_biternary cuda'
-ls yamls/models/NextToken/v6/*.yaml | parallel --tmux -j4 'export CUDA_VISIBLE_DEVICES=$(({%}%2)); python train.py {} mp_20_biternary cuda --pilot'
+ls yamls/models/NextToken/v6/*.yaml | parallel --tmux -j4 'export CUDA_VISIBLE_DEVICES=$(({%}%2)); python train.py {} mp_20_biternary cuda'
